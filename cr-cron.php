fff7e4ab06b1125fafdb8e6473d7147814d257af
sabirmostofa <sabirmostofa@gmail.com>
2012-07-17 23:58:24 +0600

    importing

--- cr-cron.php
+++ cr-cron.php
@@ -1,61 +1,62 @@
 <?php
 
-set_time_limit(1700);
+set_time_limit(3600);
 
 require_once '../../../wp-load.php';
-$url = 'http://www.warriorforum.com/warrior-special-offers-forum/';
-$content = file_get_contents($url);
-$doc = new DOMDocument();
-@$doc->loadHTML($content);
 
-/*
-$list = get_option('wso-imported-ids');
-array_pop($list);
-array_pop($list);
-array_pop($list);
+//get some proxy lists
 
-update_option('wso-imported-ids', $list);
-*/
-foreach ($doc->getElementsByTagName('a') as $single):
-    $id = $single->getAttribute('id');
-    if (stripos($id, 'thread_title') !== false):
-        if (preg_match('/\d+/', $id, $matches))
-            $id_int = $matches[0];
-        $list = get_option('wso-imported-ids');
-        if (!is_array($list))
-            $list = array();
-        if (!in_array($id_int, array(66, 313426, 142687, 122868, 25379, 4740)) && !in_array($id_int, $list)):
-            $title = html_entity_decode($single->nodeValue);
-            $post_content = '<p class="wso-extra">Above you will see the place where you can actually grade this particular WSO. If you want to see the WSO itself and validate that this is the correct WSO to grade, you can click the direct link to the WSO below. Grades are based on a typical A through F scale, so give this WSO whatever you think it deserves! Also, if you have minute, drop us a comment below about the WSO so other potential buyers can see if it is worth purchasing.</p>';
-           
-            $post_image_target = $single->getAttribute('href');
-            $post_image_src = "http://images.shrinktheweb.com/xino.php?stwembed=1&stwaccesskeyid=9690a743e365c89&stwsize=xlg&stwinside=1&stwurl=$post_image_target";
-                   
-            $image_div = <<<TY
-         <div class="wsos-post-scr" style="margin:10px auto">
-         <a target="_blank"  href="$post_image_target"><img src="$post_image_src"/></a>
-         </div>
-TY;
-            $post_content .= $image_div;
-            $text_link = '<b>Link:</b><a target="_blank" href="' . $single->getAttribute('href') . '">' . $single->getAttribute('href') . '</a>';
-            $post_content .= $text_link;
-            
-            $extra_text = '<p class="wso-ins2">Are you a WSO seller who wants to help potential purchasers to know they are buying a quality product that you stand behind? If so, grab the “report card” code below and add it to your WSO thread in the same way that you would grab an image! This will be a great way for you to let Warrior know you are proud of your product!</p>';
-            
-            $post_content.= $extra_text;
-            $new_post = array();
-            $new_post['post_title'] = $title;
-            $new_post['post_author'] = 69;
-            $new_post['post_content'] = $post_content;
-            $new_post['post_status'] = 'publish';
-            $new_post['post_category'] = array(1);
-            $new_post_id = wp_insert_post($new_post);
-            if ($new_post_id) {
-                $list[] = $id_int;
-                update_option('wso-imported-ids', $list);
+global $wpRentalImport, $wpdb;
+
+$all_cities = $wpdb->get_results("select city_url,id from $wpRentalImport->table");
+
+
+
+
+//starting the loop
+foreach ($all_cities as $city):
+    $url = $city->city_url . 'apa/';
+    $res = $wpRentalImport->get_content_direct($url);
+    if ($res[1] == 200) {
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($res[0]);
+
+        foreach ($dom->getElementsByTagName('p') as $p) {
+            if ($p->getAttribute('class') == 'row') {
+                $p_url = $p->getElementsByTagName('a')->item(0)->getAttribute('href');
+                preg_match('~\d+~', $p_url, $matches);
+                $p_id = $matches[0];
+
+
+                //if the content isn't inserted previously continue
+
+                if ($wpRentalImport->not_inserted_before($p_id, $city->id)):
+                    $l_dom = new DOMDocument();
+                    $p_title = $wpRentalImport->reform_title($p->getElementsByTagName('a')->item(0)->textContent);
+                    $pre_content = $wpRentalImport->get_content_direct($p_url);
+                    
+                    //if response received
+                    if ($pre_content[1] == 200) {
+                        @$l_dom->loadHTML($pre_content[0]);
+                        $title = $wpRentalImport->reform_title($l_dom->getElementsByTagName('h2')->item(0)->textContent);                        
+                        $body = $l_dom->saveXML($l_dom->getElementById('userbody'));                        
+                        echo $title,'<br/>', $body;
+                        break;
+                    }
+
+                endif;
             }
+        }
+    }
+    break;
 
-        endif;
-
-    endif;
 endforeach;
+
+//var_dump($all_cities);
+
+
+
+
+
+
--- main.php
+++ main.php
@@ -23,6 +23,7 @@ class wpRentalImport {
         global $wpdb;
         //$this->set_meta();
         $this->table = $wpdb->prefix . 'rental_city_list';
+        $this->table_data = $wpdb->prefix . 'rental_data';
         $this->image_dir = plugins_url('/', __FILE__) . 'images/';
         add_action('admin_enqueue_scripts', array($this, 'admin_scripts'));
         add_action('wp_enqueue_scripts', array($this, 'front_scripts'));
@@ -85,11 +86,20 @@ class wpRentalImport {
     function create_table() {
         global $wpdb;
         $sql = "CREATE TABLE IF NOT EXISTS $this->table  (
-		`id` int unsigned NOT NULL AUTO_INCREMENT,		
+		`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,		
 		`city_name` varchar(30)  NOT NULL,	
 		`city_url` varchar(60)  NOT NULL,	
 		 PRIMARY KEY (`id`),				 	
 		 key `city_name`(`city_name`)		 	
+		)";
+
+        $sql1 = "CREATE TABLE IF NOT EXISTS $this->table_data  (
+		`city_id` bigint(20) unsigned NOT NULL,		
+		`post_id` bigint(20) unsigned NOT NULL,		
+		`cg_id` bigint(20) unsigned NOT NULL,			
+		 PRIMARY KEY (`city_id`),				 	
+		 key `post_id`(`post_id`),		 	
+		 key `cg_id`(`cg_id`)		 	
 		)";
 
 
@@ -97,6 +107,7 @@ class wpRentalImport {
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
         dbDelta($sql);
+        dbDelta($sql1);
 
 
         // Adding primary cities to database
@@ -121,4 +132,153 @@ class wpRentalImport {
     }
 
 // end of create_table
+
+
+    function not_inserted_before($id, $city) {
+        global $wpdb;
+        $in = $wpdb->get_var("select post_id from $this->table_data where cg_id=$id and city_id=$city");
+        if ($in == null)
+            return true;
+    }
+
+    /*     * *********
+
+      Proxy functions
+      Only usable when simple html dom is included
+     * 
+     * *************** */
+
+    //Getting Proxy List
+
+    function getIP($obj, $html) {
+
+
+        $text = str_replace("div", "span", $obj->xmltext);
+        $text = explode("span", $text);
+
+        $ip = array();
+
+        foreach ($text as $value) {
+            $value = trim($value);
+            $value = trim($value, "<");
+            $value = trim($value, ">");
+            $value = trim($value, ".");
+
+            if (empty($value))
+                continue;
+
+            if (strpos($value, "display:none")) {
+                continue;
+            }
+
+            if (strpos($value, ">")) {
+                $value = "<" . $value . ">";
+            }
+
+            $value = strip_tags($value);
+
+            $value = trim($value, ".");
+
+            if (empty($value))
+                continue;
+
+            $ip [] = $value;
+        }
+
+        if (is_array($ip)) {
+            return implode(".", $ip);
+        }
+    }
+
+    function get_proxy_list() {
+
+        include 'simple_html_dom.php';
+
+        $html = file_get_html('http://www.hidemyass.com/proxy-list/');
+
+        $proxy_array = array();
+        $counter = 0;
+        foreach ($html->find('tr') as $element) {
+            if (++$counter == 1)
+                continue;
+            $ip = $element->find('td', 1);
+            $port = trim($element->find('td', 2)->xmltext);
+            $ip = $this->getIP($ip, $html);
+            // var_dump($element->xmltext);
+            if (preg_match('~\d~', $ip) && preg_match('~\d~', $port))
+                $proxy_array[$ip] = $port;
+        }
+
+        return $proxy_array;
+    }
+
+// get content through proxy
+
+
+    function get_content_through_proxy($url, $proxy_array) {
+
+        $ip = array_rand($proxy_array);
+
+        $port = $proxy_array[$ip];
+
+
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0');
+        curl_setopt($ch, CURLOPT_PROXY, $ip);
+        curl_setopt($ch, CURLOPT_PROXYPORT, $port);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+//curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        $res = curl_exec($ch);
+        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        return array($res, $http_status);
+    }
+
+//get content directly
+// get content through proxy
+
+
+    function get_content_direct($url) {
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0');
+//    curl_setopt($ch, CURLOPT_PROXY, $ip);
+//    curl_setopt($ch, CURLOPT_PROXYPORT, $port);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+//curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        $res = curl_exec($ch);
+        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return array($res, $http_status);
+    }
+
+    function reform_title($title) {
+        $title = strip_tags($title);
+        // Preserve escaped octets.
+        $title = preg_replace('|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title);
+        // Remove percent signs that are not part of an octet.
+        $title = str_replace('%', '', $title);
+        // Restore octets.
+        $title = preg_replace('|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title);
+        //$title = strtolower($title);
+        $title = preg_replace('/&.+?;/', '', $title); // kill entities
+        //$title = str_replace('.', '-', $title);
+        $title = preg_replace('/[^%a-zA-Z0-9\'"; $%^&*()<>_\-+=`~\]\\\|.,@#!\?\[:]/', '', $title);
+        //$title = preg_replace('/\s+/', '-', $title);
+        //$title = preg_replace('|-+|', '-', $title);
+        $title = trim($title, '-');
+        $title = trim($title, '.');
+        return trim($title);
+    }
+
 }
